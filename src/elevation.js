f85a041957b1f82db3f266d1be84e51ab714c307
Franck Lagora <lagora@users.noreply.github.com>
2018-05-11 00:12:21 +0200

    added int from hex, rounding height

--- src/elevation.js
+++ src/elevation.js
@@ -2,20 +2,20 @@ export const adjustYfromHeight = scale => data =>
     ({ ...data, y: ( data.height === 0 ? scale : data.height) / 2 });
 
 export const height = {
-    1000: ({ length, scale }) => ({ hex, i }) => {
-        const int = parseInt(hex, 16);
+    1000: ({ length, scale }) => ({ hex, int, i }) => {
         let height = int;
         let max = i === 0 ? 0 : Math.ceil(i / length);
         max = max === 0 ? 1 : max;
         height = (height / max) / ( i === 0 ? 1.25 : 2);
+        // height = Math.round(height * 10) / 10;
         return ({ height: height * scale });
     },
-    100: ({ length, scale }) => ({ hex, i }) => {
-        const int = parseInt(hex, 16);
+    100: ({ length, scale }) => ({ hex, int, i }) => {
         let height = int;
         let max = i === 0 ? 0 : Math.ceil(i / length);
         max = max === 0 ? 1 : max;
         height = (height / max) / ( i === 0 ? 1.25 : 2);
+        height = Math.round(height * 10) / 10;
         return ({ height: height * scale });
     },
     1: ({ scale }) => ({ hex }) => ({ height: parseInt(hex, 16) * scale }),
