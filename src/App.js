4e28cb9bf835528c60dbc3a5d54f39b6aaf90d9d
Franck Lagora <lagora@users.noreply.github.com>
2018-05-11 00:11:04 +0200

    Demo adjustments

--- src/App.js
+++ src/App.js
@@ -13,28 +13,34 @@ export const Box = props => props.height === 0 ? false : (
     />
   );
 
+export const Camera = ({ scale }) => (
+  <React.Fragment>
+    <a-entity id="target" />
+    <a-entity
+      id="camera"
+      camera="fov: 80; zoom: 1;"
+      position="0 2000 2000"
+      orbit-controls={`
+          autoRotate: true;
+          target: #target;
+          enableDamping: true;
+          dampingFactor: 1;
+          rotateSpeed:0.125;
+          minDistance:3;
+          maxDistance: ${scale * 20};
+          `}
+      mouse-cursor="" />
+  </React.Fragment>
+);
+
 class App extends Component {
   render() {
     const hash = core.init();
     const scale = 100;
     return (
       <a-scene background="#000">
-        {core.map({ hash })(scale)(16).map(data => <Box key={JSON.stringify(data)} {...data} scale={scale} />)}
-        <a-entity id="target" />
-        <a-entity
-          id="camera"
-          camera="fov: 80; zoom: 1;"
-          position="0 2000 1000"
-          orbit-controls={`
-              autoRotate: true;
-              target: #target;
-              enableDamping: true;
-              dampingFactor: 1;
-              rotateSpeed:0.125;
-              minDistance:3;
-              maxDistance: ${scale * 20};
-              `}
-          mouse-cursor="" />
+        {core.map({ hash })(scale)(32).map(data => <Box key={JSON.stringify(data)} {...data} scale={scale} />)}
+        <Camera scale={scale} />
       </a-scene>
     );
   }
