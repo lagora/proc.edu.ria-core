a84210a65970b8750d71723cdbf534faf4ee9c3f
Franck Lagora <lagora@users.noreply.github.com>
2018-05-12 21:55:32 +0200

    adding LOD by scale

--- src/meta/index.js
+++ src/meta/index.js
@@ -2,16 +2,16 @@ import industrial from './industrial';
 
 export const lodMappingByScale = {
     1000: 0,
-    100: 0,
-    10: 1,
-    1: 0.1,
+    100: 1,
+    10: 2,
+    1: 3,
 };
 
 export const meta = ({ hash, i, length, scale }) => {
     const type = 'volume';
     const lod = lodMappingByScale[scale];
     const sector = industrial.industrySector({ i, length, scale });
-    return { type, sector };
+    return { lod, sector, type };
 };
 
 export default meta;
